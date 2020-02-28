66491a13ae41dbcd93e7e37d92ae4c301c392e52
Sultanbek Altayev <sultanbekaltayev8@gmail.com>
2020-02-28 20:19:36 +0000

    Исправлен requirements и оптимизирован основной код

--- tululu.py
+++ tululu.py
@@ -19,67 +19,88 @@ def create_parser():
     return parser
 
 
-def download_txt(txt_url, book_title, folder):
+def get_book_title(books_soup):
+    title_and_author_selector = "#content h1"
+    title_and_author = books_soup.select_one(title_and_author_selector).text.split("::")
+    book_title = title_and_author[0].strip()
+    return book_title
+
+def get_book_author(books_soup):
+    title_and_author_selector = "#content h1"
+    title_and_author = books_soup.select_one(title_and_author_selector).text.split("::")
+    book_author = title_and_author[1].strip()
+    return book_author
+
+
+def download_txt(book_title, folder):
+    book_id = book_url.split('/')[3].split('b')[1]
+    txt_url = urljoin(url, ("txt.php?id={}".format(book_id)))
     os.makedirs(folder, exist_ok=True)
     response = requests.get(txt_url, allow_redirects=False)
     response.raise_for_status()
 
     if response.status_code == 200:
-        book_path = os.path.join(folder, sanitize_filename(book_title+'.txt'))
+        book_filename = sanitize_filename("{}{}".format(book_title,".txt"))
+        book_path = os.path.join(folder, book_filename)
         with open(book_path, 'wb') as file:
             file.write(response.content)
-        books_characteristics['book_path'] = book_path
         return book_path
 
 
-def download_image(img_url, image_name, folder):
+def download_image(books_soup, folder):
+    image_link_selector = ".bookimage a img"
+    img_src = books_soup.select_one(image_link_selector)['src']
+    image_link = urljoin(url, img_src)
+    image_name = img_src.split('/')[2]
     os.makedirs(folder, exist_ok=True)
-    response = requests.get(img_url, allow_redirects=False)
+    response = requests.get(image_link, allow_redirects=False)
     response.raise_for_status()
 
-    if response.status_code == 200:
-        image_src = os.path.join(folder, sanitize_filename(image_name))
-        with open(image_src, 'wb') as file:
-            file.write(response.content)
-        books_characteristics['image_src'] = image_src
-        return image_src
+    image_path = os.path.join(folder, sanitize_filename(image_name))
+    with open(image_path, 'wb') as file:
+        file.write(response.content)
+    return image_path
 
 
 def create_json_catalogue(json_catalogue):
     with open(json_catalogue, "w", encoding='utf8') as my_file:
         json.dump(books_catalogue,my_file, indent="   ",ensure_ascii=False)
 
+def get_comments(books_soup):
+    comments_selector = ".texts .black"
+    comments_blocks = books_soup.select(comments_selector)
+    comments = [comment.text for comment in comments_blocks]
+    return comments
+
+
+def get_genres(books_soup):
+    genres_selector = "span.d_book a"
+    genres_blocks = books_soup.select(genres_selector)
+    genres = [genre.text for genre in genres_blocks]
+    return genres
 
 if __name__ == "__main__":
     load_dotenv()
-    books_folder = os.getenv('books_folder')
-    images_folder = os.getenv('images_folder')
-    json_catalogue = os.getenv('json_catalogue')
+    books_folder = os.getenv('BOOKS_FOLDER')
+    images_folder = os.getenv('IMAGES_FOLDER')
+    json_catalogue = os.getenv('JSON_CATALOGUE')
 
     url = 'http://tululu.org'
     books_catalogue = []
-
-    parser = create_parser()
-    namespace = parser.parse_args(sys.argv[1:])
+    parser = create_parser().parse_args()
 
 
-    for page_num in range(namespace.start_page, namespace.end_page):
+    for page_num in range(parser.start_page, parser.end_page):
         page_url = urljoin(url, ('l55/{page_num}/'.format(page_num=page_num)))
         page_response = requests.get(page_url, allow_redirects=False)
-        page_response.raise_for_status()
-
-        if page_response.status_code == 200:
+        if not page_response.status_code != 200:
             page_soup = BeautifulSoup(page_response.text, 'lxml')
 
             books_selector = ".bookimage a"
             books_blocks = page_soup.select(books_selector)
 
-
             for book in books_blocks:
-                books_urls = []
-                book_link = urljoin(url, book['href'])
-                books_urls.append(book_link)
-
+                books_urls = [urljoin(url, book['href'])]
 
                 for book_url in books_urls:
                     response = requests.get(book_url, allow_redirects=False)
@@ -89,46 +110,17 @@ if __name__ == "__main__":
                     books_soup = BeautifulSoup(response.text, 'lxml')
 
 
-                    book_id = book_url.split('/')[3].split('b')[1]
-
-                    title_and_author_selector = "#content h1"
-                    title_and_author = books_soup.select_one(title_and_author_selector).text.split("::")
-                    book_title = title_and_author[0].strip()
-                    book_author = title_and_author[1].strip()
-
-                    books_characteristics = {'title': book_title}
-                    books_characteristics['author'] = book_author
-
-
-                    image_link_selector = ".bookimage a img"
-                    img_src = books_soup.select_one(image_link_selector)['src']
-                    image_link =  urljoin(url,img_src)
-                    image_name = img_src.split('/')[2]
-
-
-                    download_image(image_link, image_name, os.path.join(images_folder))
-
-                    download_txt((urljoin(url, ("txt.php?id={id}".format(id=book_id)))), book_title, os.path.join(books_folder))
-
-
-                    comments_selector = ".texts .black"
-                    comments_blocks = books_soup.select(comments_selector)
-                    comments = []
-                    for comment in comments_blocks: comments.append(comment.text)
-
-                    books_characteristics['comments'] = comments
-
-
-                    genres_selector = "span.d_book a"
-                    genres_blocks = books_soup.select(genres_selector)
-                    genres = []
-                    for genre in genres_blocks: genres.append(genre.text)
-
-                    books_characteristics['genres'] = genres
+                    books_characteristics = {'title': get_book_title(books_soup),
+                                             'author': get_book_author(books_soup),
+                                             'img_src': download_image(books_soup, os.path.join(images_folder)),
+                                             'book_path': download_txt(get_book_title(books_soup), os.path.join(books_folder)),
+                                             'comments': get_comments(books_soup),
+                                             'genres': get_genres(books_soup),
+                                             }
 
 
                     books_catalogue.append(books_characteristics)
-        else:
             break
+
 
     create_json_catalogue(json_catalogue)
